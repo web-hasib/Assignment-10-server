4613f88c53de9f68982e874a177e97358766ebb6
web-hasib <hasibulislam9284@gmail.com>
2025-05-22 12:23:39 +0600

    vercel deployed successfully

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ app.get("/", (req, res) => {
 });
 
 
-// user : recipe-book
-// pass : 548fNkHoHsX4PxrZ
+
 
 const uri =
-  "mongodb+srv://recipe-book:548fNkHoHsX4PxrZ@cluster0.yd2mcnb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yd2mcnb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+  // `mongodb+srv://recipe-book:548fNkHoHsX4PxrZ@cluster0.yd2mcnb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -33,7 +33,7 @@ async function run() {
     const recipeCollection = client.db("recipeDB").collection("recipes");
     const userCollection = client.db("recipeDB").collection("users");
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     app.get("/recipes", async (req, res) => {
       const cursor = recipeCollection.find();
       const recipes = await cursor.toArray();
@@ -96,6 +96,8 @@ async function run() {
 
 
     //! user section
+    //? OPTIONAL THING 
+    // i am trying to add a wishlist section 
     app.get('/users',async(req,res)=>{
       const cursor = userCollection.find();
       const users = await cursor.toArray();
@@ -115,10 +117,10 @@ async function run() {
     
     
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
